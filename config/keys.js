d92b7e0dc102953e3423442103e3789023ae44e5
Mike <munkyu0813@gmail.com>
2018-05-31 11:43:26 -0400

    connect to mongodb and configure express router

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://heroku_czzcn1j1:1l57cnkuk4iv6rqm1807n3v11v@ds139950.mlab.com:39950/heroku_czzcn1j1"
+};
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(error => console.log(error));
+
 app.get("/", (req, res) => res.send("Hellothere"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
